fce88866cc5d152399015650dc57cc53ee4b97ac
Andreas Jim-Hartmann <andreas.hartmann@becompany.ch>
2016-10-12 12:45:19 +0200

    Test Kramdown tables in context.

--- core/src/test/scala/laika/parse/kramdown/TableParsersSpec.scala
+++ core/src/test/scala/laika/parse/kramdown/TableParsersSpec.scala
@@ -17,27 +17,33 @@ class TableParsersSpec extends FlatSpec
   val defaultParser: Parser[RootElement] = rootElement
 
   "The table parser" should "parse tables" in {
-    val input = """|| a | b |
+    val input = """foo
+                   |
+                   || a | b |
                    ||---
                    || c | d |
                    || - | - |
                    || e | f |
                    ||===
                    || g | h |
-                   |""".stripMargin
+                   |
+                   |bar""".stripMargin
 
-    Parsing (input) should produce (root(Table(
-      TableHead(Seq(
-        Row(Seq(Cell(HeadCell, Seq(p("a"))), Cell(HeadCell, Seq(p("b")))))
-      )),
-      TableBody(Seq(
-        Row(Seq(Cell(BodyCell, Seq(p("c"))), Cell(BodyCell, Seq(p("d"))))),
-        Row(Seq(Cell(BodyCell, Seq(p("e"))), Cell(BodyCell, Seq(p("f")))))
-      )),
-      TableFoot(Seq(
-        Row(Seq(Cell(BodyCell, Seq(p("g"))), Cell(BodyCell, Seq(p("h")))))
-      ))
-    )))
+    Parsing (input) should produce (root(p("foo"),
+      Table(
+        TableHead(Seq(
+          Row(Seq(Cell(HeadCell, Seq(p("a"))), Cell(HeadCell, Seq(p("b")))))
+        )),
+        TableBody(Seq(
+          Row(Seq(Cell(BodyCell, Seq(p("c"))), Cell(BodyCell, Seq(p("d"))))),
+          Row(Seq(Cell(BodyCell, Seq(p("e"))), Cell(BodyCell, Seq(p("f")))))
+        )),
+        TableFoot(Seq(
+          Row(Seq(Cell(BodyCell, Seq(p("g"))), Cell(BodyCell, Seq(p("h")))))
+        ))
+      ),
+      p("bar")
+    ))
   }
 
 }
